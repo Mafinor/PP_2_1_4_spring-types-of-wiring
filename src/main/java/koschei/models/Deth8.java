a7185d5663860103faaea6b5ba941b4c8bfad140
Mafinor <i.mafinor@gmail.com>
2022-09-05 19:49:15 +0300

    Initial commit

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,14 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    /*
+    same here, we don't have to override or do anything
+     */
+    @Override
+    public String toString() {
+        return "";
+    }
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,22 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    /*
+    we don't have to put any fields or methods here, the task will be solved without deth8,
+    added for the sake of constructor autowiring
+     */
+    private Deth8 deth;
+    @Autowired
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth.toString();
     }
 }
